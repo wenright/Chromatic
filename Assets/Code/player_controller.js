bbf79cde4a4c09ce6d3fd241a5181f254f1ffa95
wenright <wenright@purdue.edu>
2014-06-12 17:10:36 -0500

    first playable commit
    Adds a player object, and a basic script that allows the user to move
    the player object with touches.

---
+++ Assets/Code/player_controller.js
@@ -0,0 +1,18 @@
+﻿//The speed at which the player moves towards the move location, should be between 0 and 1
+var LERP_SPEED : float;		//A higher number means faster movement
+
+//The last location that the player touches
+private var move_location : Vector2 = Vector2.zero;
+
+function Update () {
+	//Takes in player touches and stores the X and Y coordinates in terms of world coordinates
+	if (Input.touchCount > 0)
+		move_location = Camera.main.ScreenToWorldPoint(Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y));
+	
+	//Interpolates the location of the player from its current location to the last location touched by the user
+	transform.position = Vector2.Lerp(transform.position, move_location, LERP_SPEED);
+	
+	//Exits the application if the player pressed the back button
+	if (Input.GetButtonDown("Exit"))
+		Application.Quit();
+}
