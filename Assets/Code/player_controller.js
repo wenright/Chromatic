92336e301683f57a4350605f49fc8c2e34e84a06
Dan Cruwys <dcruwys@gmail.com>
2014-06-12 22:09:26 -0400

    collision work

--- Assets/Code/player_controller.js
+++ Assets/Code/player_controller.js
@@ -9,7 +9,10 @@ var color : Color = Color.white;
 
 //The sprite renderer component of this object
 var sprite : SpriteRenderer;
-
+var purple :  Color = Color.magenta;
+var green : Color = Color.green;
+var orange : Color = Color(1, 0.65, 0, 1);
+var timer : int = 0;
 function Start () {
 	sprite = GetComponent(SpriteRenderer);
 }
@@ -21,21 +24,49 @@ function Update () {
 	//Takes in mouse input instead
 	else if (Input.GetMouseButton(0))
 		move_location = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-	
+	//Checks if colored and counts down from 30, when 0 is reached, resets to white
+	if(timer <= 0){
+	  timer = 100;
+	  sprite.color = Color.white;
+	  color = Color.white;
+	 }
+	if(timer > 0 && color != Color.white){
+	  timer -= Time.deltaTime;
+	}
 	//Interpolates the location of the player from its current location to the last location touched by the user
 	transform.position = Vector2.Lerp(transform.position, move_location, LERP_SPEED);
 	
 	//Exits the application if the player pressed the back button
 	if (Input.GetButtonDown("Exit"))
 		Application.Quit();
+	
+		
+		
 }
 
 function OnTriggerEnter2D (other : Collider2D) {
 	if (other.tag == "Friendly") {
 		if (color == Color.white)
 			color = other.GetComponent(friendly).color;
-		else
-			print("todo: handle secondary colors.");
+		else if(color == Color.blue){
+			if(other.GetComponent(friendly).color == Color.red)
+				color = purple;
+			else if(other.GetComponent(friendly).color == Color.yellow)
+				color = green;
+		}
+		else if(color == Color.yellow){
+			if(other.GetComponent(friendly).color == Color.red)
+				color = orange;
+			else if(other.GetComponent(friendly).color == Color.blue)
+				color = green;
+		}
+		else if(color == Color.red){
+			if(other.GetComponent(friendly).color == Color.yellow)
+				color = orange;
+			else if(other.GetComponent(friendly).color == Color.blue)
+				color = purple;
+		}
+			
 		
 		sprite.color = color;
 		
