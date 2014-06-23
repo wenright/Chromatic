15f1c6e683d7d13b6cfe32a589b8f03a4b61d9f9
wenright <wenright@purdue.edu>
2014-06-23 17:54:13 -0500

    Added a few things to prepare for global highscores

--- Assets/Code/menu.js
+++ Assets/Code/menu.js
@@ -1,4 +1,6 @@
-﻿function OnGUI () {
+﻿var player_name : String;
+
+function OnGUI () {
 	if (PlayerPrefs.GetInt("first_time") == 0) {
 		GUI.Box(Rect(0, 0, Screen.width, Screen.height), "Looks like this is your first time, want to play the tutorial?");
 	
@@ -11,9 +13,17 @@
 			PlayerPrefs.SetInt("first_time", 1);
 	}
 	else {
-		if (GUI.Button(Rect(Screen.width / 2, 50, 60, 30), "Play"))
+		if (GUI.Button(Rect(Screen.width / 2, 50, 60, 30), "Play")) {
+			PlayerPrefs.SetString("Name", player_name);
 			Application.LoadLevel(2);
+		}
 	}
+	
+	player_name = GUI.TextField(Rect(Screen.width / 2, 120, 60, 30), player_name);
+}
+
+function Start () {
+	player_name = PlayerPrefs.GetString("Name");
 }
 
 function Update () {
