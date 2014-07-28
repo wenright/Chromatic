1dc8936d8fbdfcaef79f3ad21b14f6539080cb01
Dan Cruwys <dcruwys@gmail.com>
2014-07-27 20:03:10 -0400

    deleted somethings added buttons

--- Assets/Code/Menu Scripts/options.js
+++
@@ -1,9 +0,0 @@
-﻿var explosion : GameObject;
-
-function OnCollisionEnter2D () {
-	var e : GameObject = Instantiate(explosion, transform.position, transform.rotation);
-	e.GetComponent(ParticleSystem).startColor = Color.blue;
-	yield WaitForSeconds(1);
-	
-	//Put option sutff here
-}
