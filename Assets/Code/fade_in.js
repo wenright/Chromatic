d7083af3d1e9e4fc9681db51af31cdf28978c54b
wenright <wenright@purdue.edu>
2014-07-27 19:59:28 -0500

    Menu buttons
    It looks a little weird, I will try to make the buttons fade with the
    fader.

--- Assets/Code/fade_in.js
+++ Assets/Code/fade_in.js
@@ -1,5 +1,5 @@
-﻿var otherThingToFade : GameObject;
-var spawnerScript : spawner;
+﻿var spawnerScript : spawner;
+var menuButtons : GameObject;
 
 private var done : boolean = false;
 
@@ -7,19 +7,16 @@ function Start () {
 	done = false;
 }
 
-function Update () {
-	if ((Input.GetButtonDown("Fire1") || Input.touchCount > 0) && !done)
-		Fade ();
-}
-
 function Fade () {
 	done = true;
 	
 	for (var i : int = 0; i < 75; i++) {
 		transform.GetComponent(GUITexture).color.a -= 0.03;
-		otherThingToFade.transform.GetComponent(TextMesh).color.a -= 0.1;
 		yield WaitForSeconds(0.02);
 	}
 	
-	spawnerScript.Spawn ();
+	menuButtons.SetActive (false);
+	
+	if (spawnerScript)
+		spawnerScript.Spawn ();
 }
