b9329fc8a0fa2e3ad8d98673beb264547850d538
Dan Cruwys <dcruwys@gmail.com>
2014-08-27 23:36:03 -0400

    added text to menu

--- Assets/Code/fade_in.js
+++ Assets/Code/fade_in.js
@@ -17,16 +17,25 @@ function Fade () {
 	
 	for (var i : int = 0; i < 15; i++) {
 		transform.GetComponent(GUITexture).color.a -= 0.03;
-		for (var j : int = 0; j < menuButtons.Length; j++)
-			menuButtons[j].GetComponent(SpriteRenderer).color.a -= 0.1;
+		for (var j : int = 0; j < menuButtons.Length; j++){
+			if(menuButtons[j].GetComponent(SpriteRenderer))
+				menuButtons[j].GetComponent(SpriteRenderer).color.a -= 0.1;
+			else
+				menuButtons[j].GetComponent(TextMesh).color.a -= 0.1;
+		}
 		
 		yield WaitForSeconds(0.01);
 	}
 	if (pauser)
 		pauser.canPause = true;
 	transform.GetComponent(GUITexture).color.a = 0;
-	for (var k : int = 0; k < menuButtons.Length; k++)
-		menuButtons[k].GetComponent(SpriteRenderer).color.a = 0;
+	for (var k : int = 0; k < menuButtons.Length; k++){
+		if(menuButtons[k].GetComponent(SpriteRenderer))
+			menuButtons[k].GetComponent(SpriteRenderer).color.a = 0;
+		else 
+			menuButtons[k].GetComponent(TextMesh).color.a = 0;
+		
+		}
 	
 	if (player_C)
 		player_C.canMove = true;
