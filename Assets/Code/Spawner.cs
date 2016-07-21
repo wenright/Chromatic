b8569b63d3f34bd75e00ad31594bf4177862dc9c
Dan Cruwys <dan.cruwys@du.edu>
2016-07-20 19:37:00 -0600

    Added some groundwork for Spawners

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn(int i)    {
+        SpawnPattern current = new SpawnPattern(i);
+    }
+}
