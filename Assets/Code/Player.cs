b7d37d5ce77aa9bff07da33e621ac7fc491a9c32
Will Enright <Will Enright>
2016-07-20 21:33:00 -0400

    Add player prefab and basic movement

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private int moveSpeed = 20;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0)) {
+            Vector2 moveLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.MoveTowards(transform.position, moveLocation, Time.deltaTime * moveSpeed);
+        }
+#else
+    
+#endif
+    }
+}
