26ba7b5197ffc1004384f5ba7c361f140ae4765d
Will Enright <wenright@purdue.edu>
2016-07-22 17:46:03 -0400

    Add Colors class

---
+++ Assets/Code/Colors.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colors : MonoBehaviour {
+    public Color white = Color.white;
+    public Color purple = new Color(160 / 255.0F, 0, 240 / 255.0F);
+    public Color green = new Color(20/255.0F, 220/255.0F, 0);
+    public Color orange = new Color(1, 127 / 255.0F, 0);
+    public Color red = Color.red;
+    public Color yellow = Color.yellow;
+    public Color blue = Color.blue;
+}
