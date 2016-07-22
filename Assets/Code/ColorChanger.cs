fcae6add48f245656aca4dbba41908172a4560f5
Will Enright <wenright@purdue.edu>
2016-07-22 17:26:14 -0400

    Add color changer prefab

---
+++ Assets/Code/ColorChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D () {
+        // TODO change player color to w/e this is (Or calculate new color based on players color)
+    }
+}
