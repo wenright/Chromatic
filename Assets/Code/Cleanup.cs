7791b09e4b6395fe0850f717efdf1be96f187b13
Will Enright <wenright@purdue.edu>
2016-07-22 23:13:07 -0400

    Fix cleanup script to remove particle systems

--- Assets/Code/Cleanup.cs
+++ Assets/Code/Cleanup.cs
@@ -6,11 +6,6 @@ public class Cleanup : MonoBehaviour {
     public float delay;
 
 	void Awake () {
-        Destroy();
-	}
-
-    IEnumerator Destroy () {
-        yield return new WaitForSeconds(delay);
-        Destroy(gameObject);
+        Destroy(gameObject, delay);
     }
 }
