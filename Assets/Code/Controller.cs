c33fc8b652d53e86210df06086151e6bebc13777
wenright <wenright@purdue.edu>
2017-04-02 13:06:05 -0500

    Add highscore package

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -5,73 +5,90 @@ using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour {
 
-	//Singleton
-	private static Controller gcsingleton;
-	public static Controller Instance { get { return gcsingleton; } }
+    //Singleton
+    private static Controller gcsingleton;
+    public static Controller Instance { get { return gcsingleton; } }
 
     // UI controls
     public Slider healthSlider; public float hp; public readonly float  MAX_HP = 200;
-	public Text waveText;
-	public Text scoreText;
+    public Text waveText;
+    public Text scoreText;
 
     public Image Fill;  // assign in the editor the "Fill"
     private Player player;
-	private Color bgcolor;
+    private Color bgcolor;
 
     public int level;
-	public int wavecounter;
+    public int wavecounter;
 
-	public int score;
-	public int multipler;
+    public int score;
+    public int multipler;
 
+    private LeaderboardController leaderboardController;
 
     void Awake () {
-		wavecounter = 0;
-		if (gcsingleton == null) {
-			gcsingleton = this;
-		}
-		else {
-			Destroy (gameObject);
-		}
-        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        wavecounter = 0;
+
+        if (gcsingleton == null) {
+            gcsingleton = this;
+        } else {
+            Destroy (gameObject);
+        }
+
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null) {
+            player = playerObject.GetComponent<Player>();
+        } else {
+            Debug.LogError("Unable to find Player object!");
+        }
+
+        leaderboardController = new LeaderboardController();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		waveText.text = "Wave " + wavecounter;
-		scoreText.text = score.ToString ();
-		if (healthSlider) {
-			healthSlider.value = hp / MAX_HP;
-			Fill.color = player.GetColor ();
-		}
+    
+    void Update () {
+        waveText.text = "Wave " + wavecounter;
+        scoreText.text = score.ToString ();
+
+        if (healthSlider) {
+            healthSlider.value = hp / MAX_HP;
+            Fill.color = player.GetColor ();
+        }
+
         if (hp > 0) {
             hp--;
         }
 
-		if (player.GetColor () == ColorList.purple || player.GetColor () == ColorList.orange || player.GetColor () == ColorList.green) {
-			ChangeBGColor (player.GetColor ());
-		} else {
-			ChangeBGColor (ColorList.white);
-		}
+        if (player.GetColor () == ColorList.purple || player.GetColor () == ColorList.orange || player.GetColor () == ColorList.green) {
+            ChangeBGColor (player.GetColor ());
+        } else {
+            ChangeBGColor (ColorList.white);
+        }
     }
-	void ChangeBGColor (Color color) {
-		if (color == Color.white)
-			GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ().backgroundColor = color / 6;
-		else
-			GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ().backgroundColor = color / 4;
-	}
-	public void SetLevel(int i){
-		if (i != level) {
-			wavecounter++;
-			level = i;
-		}
-	}
-	public void IncreaseScore(){
-		multipler++;
-		score = score + (multipler * 50);
-	}
 
-	public void RestMultiplier(){
-		multipler = 0;
-	}
+    void ChangeBGColor (Color color) {
+        if (color == Color.white)
+            GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ().backgroundColor = color / 6;
+        else
+            GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ().backgroundColor = color / 4;
+    }
+
+    public void SetLevel(int i){
+        if (i != level) {
+            wavecounter++;
+            level = i;
+        }
+    }
+
+    public void IncreaseScore(){
+        multipler++;
+        score = score + (multipler * 50);
+    }
+
+    public void ResetMultiplier(){
+        multipler = 0;
+    }
+
+    public void UploadScore () {
+        leaderboardController.UploadScore(score);
+    }
 }
---
+++ Assets/Code/LeaderboardController.cs
@@ -0,0 +1,43 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class LeaderboardController {
+
+	private readonly string leaderboardID = "CgkIz8WKy80eEAIQAQ";
+
+	// Use this for initialization
+	public LeaderboardController () {
+		if (!Social.localUser.authenticated) {
+			PlayGamesPlatform.DebugLogEnabled = true;
+			PlayGamesPlatform.Activate();
+			
+			PlayGamesPlatform.Instance.Authenticate((bool success) => {
+				// handle success or failure
+				Debug.Log("name: " + Social.localUser.userName + ", state: " + Social.localUser.state + ", authenticated: " + Social.localUser.authenticated + ", id: " + Social.localUser.id);
+
+				if (success) {
+					Debug.Log("Successfully signed in");
+				} else {
+					Debug.LogError("Failed signing in!");
+				}
+			});
+		}
+	}
+	
+	public void UploadScore (int score) {
+		Social.ReportScore(score, leaderboardID, (bool success) => {
+			// handle success or failure
+			if (success) {
+				Debug.Log("Successfully uploaded a high score");
+			} else {
+				Debug.LogError("Failed uploading high score to leaderboards!");
+			}
+		});
+	}
+
+	public void ShowLeaderboardUI () {
+		PlayGamesPlatform.Instance.ShowLeaderboardUI(leaderboardID);
+	}
+}
