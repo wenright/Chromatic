c3d8eb5b29053f915cd2918214d5be59f34f570d
wenright <wenright@purdue.edu>
2017-04-06 16:04:10 -0500

    Play video ad after every time you die

---
+++ Assets/Code/AdvertisementManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdvertisementManager : MonoBehaviour
+{
+  public void Start()
+  {
+    // TODO only play an ad after every 3 deaths or so
+    if (Advertisement.IsReady())
+    {
+      Advertisement.Show();
+    }
+  }
+}
